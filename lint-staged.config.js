bca3523b2a646af69559aff499c266b1f1616176
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2023-06-20 00:13:28 -0300

    Force absolute paths

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 // lint-staged.config.js
 module.exports = {
-  '!(src/__generated__/*.graphql.ts|src/assets/*.png)*.ts?(x)': files => {
+  '!(src/__generated__/*.graphql.ts|src/assets/*.png)*.ts?(x)': (files) => {
     const fileNames = files.join(' ');
 
     return [`eslint ${fileNames}`, `prettier -w ${fileNames}`];
