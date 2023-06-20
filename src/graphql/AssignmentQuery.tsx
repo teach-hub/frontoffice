68eed9ea6304a5e65a043d98e1c290613a9cca10
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2023-06-20 14:39:34 -0300

    Add missing id

--- src/graphql/AssignmentQuery.tsx
+++ src/graphql/AssignmentQuery.tsx
@@ -3,6 +3,7 @@ import { graphql } from 'babel-plugin-relay/macro';
 export default graphql`
   query AssignmentQuery($id: ID!, $courseId: ID!) {
     viewer {
+      id
       course(id: $courseId) {
         id
         assignment(id: $id) {
