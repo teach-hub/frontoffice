6885033ee393900700ee24747312203446a32b3b
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2023-06-25 01:27:47 -0300

    Add'em

--- src/components/ListIcon.tsx
+++ src/components/ListIcon.tsx
@@ -1,9 +1,16 @@
 import { Icon } from '@primer/octicons-react/dist/icons';
 import { theme } from 'theme';
-import { ListIcon as ChakraListIcon } from '@chakra-ui/react';
+import { IconProps, ListIcon as ChakraListIcon } from '@chakra-ui/react';
 
-const ListIcon = ({ icon }: { icon: Icon }) => (
-  <ChakraListIcon as={icon} color={theme.colors.teachHub.primary} boxSize={'6'} />
+type Props = IconProps & { icon: Icon };
+
+const ListIcon = ({ icon, ...rest }: Props) => (
+  <ChakraListIcon
+    as={icon}
+    color={theme.colors.teachHub.primary}
+    boxSize={'6'}
+    {...rest}
+  />
 );
 
 export default ListIcon;
