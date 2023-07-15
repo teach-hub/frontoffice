06d11e01a90bd8cb36d2a8cc704888c3d215c91d
Sebastian Penna <31221128+sebapenna@users.noreply.github.com>
2023-07-15 15:06:54 -0300

    TH-149 Join group as student (#37)

---
+++ src/graphql/JoinGroupMutation.tsx
@@ -0,0 +1,15 @@
+import { graphql } from 'babel-plugin-relay/macro';
+
+export default graphql`
+  mutation JoinGroupMutation($groupId: ID!, $courseId: ID!, $assignmentId: ID!) {
+    joinGroup(groupId: $groupId, courseId: $courseId, assignmentId: $assignmentId) {
+      id
+      group {
+        id
+        name
+        courseId
+      }
+      assignmentId
+    }
+  }
+`;
