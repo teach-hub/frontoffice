b2ee4670a4b637746c90da9bde34ac75b0478cf2
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2023-08-07 00:37:26 -0300

    Improve screen

--- src/pages/courses/assignments/AssignmentDashboard.tsx
+++ src/pages/courses/assignments/AssignmentDashboard.tsx
@@ -18,6 +18,7 @@ import { Permission, useUserContext } from 'hooks/useUserCourseContext';
 
 import Navigation from 'components/Navigation';
 import IconButton from 'components/IconButton';
+import Button from 'components/Button';
 import PageDataContainer from 'components/PageDataContainer';
 import Heading from 'components/Heading';
 import List from 'components/list/List';
@@ -26,10 +27,151 @@ import Card from 'components/Card';
 
 import AssignmentQueryDef from 'graphql/AssignmentQuery';
 
-import type { AssignmentQuery } from '__generated__/AssignmentQuery.graphql';
+import type {
+  AssignmentQuery,
+  AssignmentQuery$data,
+} from '__generated__/AssignmentQuery.graphql';
 import { DateListItem } from 'components/list/DateListItem';
 import { TextListItem } from 'components/list/TextListItem';
 import { LinkListItem } from 'components/list/LinkListItem';
+
+type Course = NonNullable<NonNullable<AssignmentQuery$data['viewer']>['course']>;
+type Assignment = NonNullable<Course['assignment']>;
+
+const LIST_ITEM_ICON_COLOR = theme.colors.teachHub.white;
+
+function AssignmentDetails({ assignment }: { assignment: Assignment }) {
+  const courseContext = useUserContext();
+
+  const viewerCanSubmit =
+    assignment.isOpenForSubmissions && !assignment.viewerAlreadyMadeSubmission;
+
+  return (
+    <Card>
+      <List p="30px">
+        {courseContext.userHasPermission(Permission.SubmitAssignment) && (
+          <TextListItem
+            label="Entrega: "
+            listItemKey="entrega"
+            iconProps={{
+              color: LIST_ITEM_ICON_COLOR,
+              icon: StarIcon,
+            }}
+            text={assignment.viewerAlreadyMadeSubmission ? 'Entregado' : 'No entregado'}
+          />
+        )}
+        <TextListItem
+          iconProps={{
+            color: LIST_ITEM_ICON_COLOR,
+            icon: assignment.isGroup ? PeopleIcon : PersonIcon,
+          }}
+          text={assignment.isGroup ? 'Entrega grupal' : 'Entrega individual'}
+          listItemKey={'isGroup'}
+        />
+        <DateListItem
+          date={assignment.startDate}
+          label={'Inicio de entregas: '}
+          listItemKey={'startDate'}
+          iconColor={LIST_ITEM_ICON_COLOR}
+        />
+        <DateListItem
+          date={assignment.endDate}
+          label={'Fecha límite de entregas: '}
+          listItemKey={'endDate'}
+          iconColor={LIST_ITEM_ICON_COLOR}
+        />
+        {courseContext.userIsTeacher && (
+          <TextListItem
+            listItemKey={'allowLateSubmissions'}
+            iconProps={{
+              color: LIST_ITEM_ICON_COLOR,
+              icon: AlertIcon,
+            }}
+            text={assignment.allowLateSubmissions ? 'Permitidas' : 'No Permitidas'}
+            label={'Entregas fuera de fecha: '}
+          />
+        )}
+        {assignment.link ? (
+          <LinkListItem
+            listItemKey={'link'}
+            iconColor={LIST_ITEM_ICON_COLOR}
+            link={assignment.link}
+            text={'Ver enunciado'}
+            external={true}
+          />
+        ) : (
+          <></>
+        )}
+        {courseContext.userIsTeacher && (
+          <LinkListItem
+            listItemKey={'submissions'}
+            iconColor={LIST_ITEM_ICON_COLOR}
+            external={false}
+            text={'Ver entregas'}
+            link={'submissions'}
+          />
+        )}
+        {courseContext.userHasPermission(Permission.AssignReviewer) && (
+          <LinkListItem
+            listItemKey={'assignReviewers'}
+            iconColor={LIST_ITEM_ICON_COLOR}
+            external={false}
+            text={'Assignar correctores'}
+            link={'assign-reviewers'}
+          />
+        )}
+        {courseContext.userHasPermission(Permission.SubmitAssignment) && (
+          <LinkListItem
+            listItemKey={'addSubmission'}
+            iconColor={LIST_ITEM_ICON_COLOR}
+            external={false}
+            text={
+              assignment.viewerAlreadyMadeSubmission
+                ? 'Entrega realizada'
+                : 'Realizar nueva entrega'
+            }
+            link={'submissions/add'}
+            disabled={!viewerCanSubmit}
+          />
+        )}
+      </List>
+    </Card>
+  );
+}
+
+function AssignmentActions() {
+  const courseContext = useUserContext();
+  const navigate = useNavigate();
+
+  const actions = [];
+
+  if (courseContext.userHasPermission(Permission.EditAssignment)) {
+    actions.push(
+      <Button leftIcon={<PencilIcon />} onClick={() => navigate(`edit`)}>
+        Editar
+      </Button>
+    );
+  }
+
+  if (courseContext.userHasPermission(Permission.DeleteAssignment)) {
+    actions.push(
+      <Button
+        // TODO: TH-93 Delete assignments (with a confirmation pop up)
+        onClick={() => null}
+        leftIcon={<TrashIcon />}
+        color={theme.colors.teachHub.red}
+      >
+        Eliminar
+      </Button>
+    );
+  }
+
+  return (
+    <Flex gap="5px" direction={'row'}>
+      {actions}
+    </Flex>
+  );
+}
 
 const AssignmentDashboardPage = ({
   assignmentId,
@@ -38,9 +180,6 @@ const AssignmentDashboardPage = ({
   assignmentId: string;
   courseId: string;
 }) => {
-  const navigate = useNavigate();
-  const courseContext = useUserContext();
-
   const data = useLazyLoadQuery<AssignmentQuery>(AssignmentQueryDef, {
     id: assignmentId,
     courseId,
@@ -52,130 +191,17 @@ const AssignmentDashboardPage = ({
     return null;
   }
 
-  const LIST_ITEM_ICON_COLOR = theme.colors.teachHub.white;
-
-  const viewerCanSubmit =
-    assignment.isOpenForSubmissions && !assignment.viewerAlreadyMadeSubmission;
-
   return (
-    <PageDataContainer>
-      <Heading>{assignment.title}</Heading>
-      <Flex margin="20px 0px" gap={'20px'}>
-        {courseContext.userHasPermission(Permission.EditAssignment) && (
-          <IconButton
-            onClick={() => navigate(`edit`)}
-            aria-label={'Edit'}
-            icon={<PencilIcon size={'medium'} />}
-            variant={'ghost'}
-            color={theme.colors.teachHub.black}
-          />
-        )}
-        {courseContext.userHasPermission(Permission.DeleteAssignment) && (
-          <IconButton
-            // TODO: TH-93 Delete assignments (with a confirmation pop up)
-            onClick={() => null}
-            aria-label={'Delete'}
-            icon={<TrashIcon size={'medium'} />}
-            variant={'ghost'}
-            color={theme.colors.teachHub.red}
-          />
-        )}
+    <PageDataContainer w="70em" gap="25px">
+      <Flex gap="50px" justifyContent={'space-between'} alignItems={'center'}>
+        <Heading textOverflow={'ellipsis'} overflow={'hidden'} whiteSpace="nowrap">
+          {assignment.title}
+        </Heading>
+        <AssignmentActions />
       </Flex>
-      <Flex direction={'row'}>
-        <Text marginEnd="80px" w={'600px'} whiteSpace="pre-wrap">
-          {assignment.description}
-        </Text>
-        <Card>
-          <List p="30px">
-            {courseContext.userHasPermission(Permission.SubmitAssignment) && (
-              <TextListItem
-                label="Entrega: "
-                listItemKey="entrega"
-                iconProps={{
-                  color: LIST_ITEM_ICON_COLOR,
-                  icon: StarIcon,
-                }}
-                text={
-                  assignment.viewerAlreadyMadeSubmission ? 'Entregado' : 'No entregado'
-                }
-              />
-            )}
-            <TextListItem
-              iconProps={{
-                color: LIST_ITEM_ICON_COLOR,
-                icon: assignment.isGroup ? PeopleIcon : PersonIcon,
-              }}
-              text={assignment.isGroup ? 'Entrega grupal' : 'Entrega individual'}
-              listItemKey={'isGroup'}
-            />
-            <DateListItem
-              date={assignment.startDate}
-              label={'Inicio de entregas: '}
-              listItemKey={'startDate'}
-              iconColor={LIST_ITEM_ICON_COLOR}
-            />
-            <DateListItem
-              date={assignment.endDate}
-              label={'Fecha límite de entregas: '}
-              listItemKey={'endDate'}
-              iconColor={LIST_ITEM_ICON_COLOR}
-            />
-            {courseContext.userIsTeacher && (
-              <TextListItem
-                listItemKey={'allowLateSubmissions'}
-                iconProps={{
-                  color: LIST_ITEM_ICON_COLOR,
-                  icon: AlertIcon,
-                }}
-                text={assignment.allowLateSubmissions ? 'Permitidas' : 'No Permitidas'}
-                label={'Entregas fuera de fecha: '}
-              />
-            )}
-            {assignment.link ? (
-              <LinkListItem
-                listItemKey={'link'}
-                iconColor={LIST_ITEM_ICON_COLOR}
-                link={assignment.link}
-                text={'Ver enunciado'}
-                external={true}
-              />
-            ) : (
-              <></>
-            )}
-            {courseContext.userIsTeacher && (
-              <LinkListItem
-                listItemKey={'submissions'}
-                iconColor={LIST_ITEM_ICON_COLOR}
-                external={false}
-                text={'Ver entregas'}
-                link={'submissions'}
-              />
-            )}
-            {courseContext.userHasPermission(Permission.AssignReviewer) && (
-              <LinkListItem
-                listItemKey={'assignReviewers'}
-                iconColor={LIST_ITEM_ICON_COLOR}
-                external={false}
-                text={'Assignar correctores'}
-                link={'assign-reviewers'}
-              />
-            )}
-            {courseContext.userHasPermission(Permission.SubmitAssignment) && (
-              <LinkListItem
-                listItemKey={'addSubmission'}
-                iconColor={LIST_ITEM_ICON_COLOR}
-                external={false}
-                text={
-                  assignment.viewerAlreadyMadeSubmission
-                    ? 'Entrega realizada'
-                    : 'Realizar nueva entrega'
-                }
-                link={'submissions/add'}
-                disabled={!viewerCanSubmit}
-              />
-            )}
-          </List>
-        </Card>
+      <Flex direction="row" justifyContent="space-between">
+        <Text w={'50%'}>{assignment.description}</Text>
+        <AssignmentDetails assignment={assignment} />
       </Flex>
     </PageDataContainer>
   );
