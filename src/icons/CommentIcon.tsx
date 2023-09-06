9f6540443b1b282d533cf210ea3839ec8106b081
spenna <sebapenna19@gmail.com>
2023-09-05 21:35:34 -0300

    TH-183 Add pull request comments to submission page and remove description

---
+++ src/icons/CommentIcon.tsx
@@ -0,0 +1,7 @@
+import { CommentDiscussionIcon, IconProps } from '@primer/octicons-react';
+
+type Props = IconProps;
+
+export default (props: Props) => {
+  return <CommentDiscussionIcon {...props} />;
+};
