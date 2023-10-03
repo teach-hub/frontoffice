47f8d33e460cfa50e6bc15e057433c4d8f6f52db
spenna <sebapenna19@gmail.com>
2023-10-03 16:05:42 -0300

    TH-112 Improve not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,33 @@
+import Navigation from 'components/Navigation';
+import { Suspense } from 'react';
+import { Center, Image, Stack } from '@chakra-ui/react';
+import Heading from 'components/Heading';
+import logo from 'assets/logo_wo_text.png';
+import Text from 'components/Text';
+
 const NotFoundPage = () => {
   return (
-    <h1> Missing page </h1>
-  )
-}
+    <Navigation>
+      <Suspense>
+        <Center height="70vh">
+          <Stack
+            width={'100%'}
+            height={'100%'}
+            direction={'column'}
+            align={'center'}
+            justify={'center'}
+            gap={20}
+          >
+            <Image src={logo} />
+            <Stack justify={'center'} alignItems={'center'}>
+              <Heading>404</Heading>
+              <Text>Page not found</Text>
+            </Stack>
+          </Stack>
+        </Center>
+      </Suspense>
+    </Navigation>
+  );
+};
 
 export default NotFoundPage;
