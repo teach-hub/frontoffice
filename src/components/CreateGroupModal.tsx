12bd36642bd8532b1f66721bf79bf219906eb1c1
spenna <sebapenna19@gmail.com>
2023-10-06 20:37:55 -0300

    TH-111 Configure spinner on create group as student page

--- src/components/CreateGroupModal.tsx
+++ src/components/CreateGroupModal.tsx
@@ -1,21 +1,20 @@
-import { Flex } from '@chakra-ui/react';
+import type { ModalProps } from '@chakra-ui/react';
 import { useMutation } from 'react-relay';
 import { useNavigate } from 'react-router-dom';
 
 import Text from 'components/Text';
 import { Modal } from 'components/Modal';
 import Button from 'components/Button';
-import InputField from 'components/InputField';
 
 import useToast from 'hooks/useToast';
 
 import CreateGroupWithParticipantMutationDef from 'graphql/CreateGroupWithParticipantMutation';
-
-import type { ModalProps } from '@chakra-ui/react';
 import type {
   CreateGroupWithParticipantMutation,
   CreateGroupWithParticipantMutation$data,
 } from '__generated__/CreateGroupWithParticipantMutation.graphql';
+import React, { useState } from 'react';
+import Spinner from 'components/Spinner';
 
 export type Props = {
   isOpen: ModalProps['isOpen'];
@@ -33,18 +32,21 @@ const CreateGroupModal = (props: Props) => {
   const toast = useToast();
   const navigate = useNavigate();
 
+  const [showSpinner, setShowSpinner] = useState<boolean>(false);
   const [commitCreateGroupWithParticipant] =
     useMutation<CreateGroupWithParticipantMutation>(
       CreateGroupWithParticipantMutationDef
     );
 
   const handleGroupChangeSubmit = () => {
+    setShowSpinner(true);
     commitCreateGroupWithParticipant({
       variables: {
         courseId,
         assignmentId: chosenAssignmentGroup?.assignmentId ?? '',
       },
       onCompleted: (response: CreateGroupWithParticipantMutation$data, errors) => {
+        setShowSpinner(false);
         const responseData = response.createGroupWithParticipant;
         const group = responseData?.group;
         if (!errors?.length && group) {
@@ -62,25 +64,33 @@ const CreateGroupModal = (props: Props) => {
   };
 
   return (
-    <Modal
-      headerText={`Trabajo Práctico: ${chosenAssignmentGroup?.assignmentTitle}`}
-      isOpen={isOpen}
-      onClose={onClose}
-      isCentered
-      footerChildren={
-        <>
-          <Button onClick={onClose} variant={'ghost'}>
-            {'Cancelar'}
-          </Button>
-          <Button onClick={handleGroupChangeSubmit}>{'Crear'}</Button>
-        </>
-      }
-    >
-      <Text fontSize={'md'}>
-        Vas a generar un nuevo grupo con un nombre autogenerado y comenzarás a formar
-        parte de él
-      </Text>
-    </Modal>
+    <>
+      <Spinner
+        isOpen={showSpinner}
+        onClose={() => {
+          setShowSpinner(false);
+        }}
+      />
+      <Modal
+        headerText={`Trabajo Práctico: ${chosenAssignmentGroup?.assignmentTitle}`}
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        footerChildren={
+          <>
+            <Button onClick={onClose} variant={'ghost'}>
+              {'Cancelar'}
+            </Button>
+            <Button onClick={handleGroupChangeSubmit}>{'Crear'}</Button>
+          </>
+        }
+      >
+        <Text fontSize={'md'}>
+          Vas a generar un nuevo grupo con un nombre autogenerado y comenzarás a formar
+          parte de él
+        </Text>
+      </Modal>
+    </>
   );
 };
 
